fc554c8b48240c64e47d8be405e950b3105af01f
JulianSupercode <jscheib1305@gmail.com>
2024-07-26 16:43:16 +0200

    nues Projekt

---
+++ 10_Project_Beer_Neu/src/pages/DetailPage.tsx
@@ -0,0 +1,14 @@
+
+import DetailBeer from "../components/DetailBeer/DetailBeer";
+import Footer from "../components/Footer/Footer";
+
+const DetailPage = () => {
+  return (
+    <div className="detail-page">
+      <DetailBeer />
+      <Footer />
+    </div>
+  );
+};
+
+export default DetailPage;
